ea8a7df9e69edd51c77a6e5a32698b27663e7618
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-05-04 09:25:45 +0100

    Update solution.py

--- Level-2/dont-get-volunteered/solution.py
+++ Level-2/dont-get-volunteered/solution.py
@@ -10,8 +10,11 @@ def get_position(src):
     return x, y
 
 def children(src):
+    # get x, y position of src
     x, y = get_position(src)
     checkboard = get_checkboard()
+    
+    # available moves
     a = (x - 2, y + 1)
     b = (x - 1, y + 2)
     c = (x + 1, y + 2)
@@ -26,6 +29,7 @@ def children(src):
         # check if out of bounds
         if (move[0] >= 0 and move[0] < 8) and (move[1] >= 0 and move[1] < 8):
             x, y = move[0], move[1]
+            # if move is legal (not out of bounds) save it
             new_pos.append(checkboard[move[0]][move[1]])
     return new_pos
 
@@ -34,13 +38,19 @@ def breadth_first_search(src, dest):
     level = {src: 0}
     
     while len(queue) > 0:
+        # dequeue
         node = queue.pop(0)
         
         for child in children(node):
+            # check if visited
             if child not in level:
                 queue.append(child)
+                # the distance of child from src is
+                # level[node] + 1, inasmuch as level[node]
+                # is the distance of src to node
                 level[child] = level[node] + 1
             
+            # have we found it?
             if child == dest:
                 return level[dest]
     return 0
